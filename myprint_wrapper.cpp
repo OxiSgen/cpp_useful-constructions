6e85ab6aaf615a54fb5d343855a64c5035af8b30
OxiSgen <60621483+OxiSgen@users.noreply.github.com>
2021-08-26 13:18:11 +0300

    Create myprint_wrapper.cpp

---
+++ myprint_wrapper.cpp
@@ -0,0 +1,7 @@
+void myprint() { std::cout << std::endl; }
+
+template<typename T, typename... Args> void myprint(const T& t, const Args& ... args)
+{
+    std::cout << t << std::endl;
+    myprint(args...);
+}
